33fbd448fd76e92b3ff595de9e20b541d9356911
Dalton Ricker <dricker@wayfair.com>
2020-08-24 10:58:38 -0400

    added other project that i had previously been playing around with

---
+++ AddWithThreads/AddWithThreads/AddParams.cs
@@ -0,0 +1,13 @@
+namespace AddWithThreads
+{
+    public class AddParams
+    {
+        public int A, B;
+
+        public AddParams(int numb1, int numb2)
+        {
+            A = numb1;
+            B = numb2;
+        }
+    }
+}
---
+++ AddWithThreads/AddWithThreads/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace AddWithThreads
+{
+    class Program
+    {
+        private static AutoResetEvent _waitHandle = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.WriteLine("***** Adding with Thread Objects *****");
+            Console.WriteLine("ID of thread in Main(): {0}", Thread.CurrentThread.ManagedThreadId);
+
+            // Make an AddParams object to pass to the secondary thread.
+            var ap = new AddParams(10, 10);
+            var t = new Thread(Add);
+            t.Start(ap);
+
+            /*// Force a wait to let the other thread finish.
+            Thread.Sleep(5);*/
+
+            // Wait here until you are notified.
+            _waitHandle.WaitOne();
+            Console.WriteLine("Other Thread is done!");
+
+            Console.ReadLine();
+        }
+
+        static void Add(object data)
+        {
+            if (data is AddParams)
+            {
+                Console.WriteLine("ID of thread in Add(): {0}", Thread.CurrentThread.ManagedThreadId);
+
+                var ap = (AddParams) data;
+                Console.WriteLine("{0} + {1} is {2}", ap.A, ap.B, ap.A + ap.B);
+
+                // Tell other thread we are done.
+                _waitHandle.Set();
+            }
+        }
+    }
+}

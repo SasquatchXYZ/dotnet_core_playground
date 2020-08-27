fe907f0af87838b30aab804bcfffccbdbb1f8130
Dalton Ricker <dricker@wayfair.com>
2020-08-27 17:49:13 -0400

    added monitoring app and library

---
+++ MonitoringApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using MonitoringLib;
+
+namespace MonitoringApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Processing. Please wait...");
+            Recorder.Start();
+
+            // simulate a process that requires some memory resources...
+            var largeArrayOfInts = Enumerable.Range(1, 10_000).ToArray();
+            
+            // ...and takes some time to complete
+            Thread.Sleep(new Random().Next(5, 10) * 1000);
+            
+            Recorder.Stop();*/
+
+            var numbers = Enumerable.Range(1, 50_000).ToArray();
+
+            Recorder.Start();
+            Console.WriteLine("Using string with +");
+            var s = "";
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                s += numbers[i] + ", ";
+            }
+
+            Recorder.Stop();
+
+            Recorder.Start();
+            Console.WriteLine("Using string with + (foreach)");
+            var a = "";
+            foreach (var t in numbers)
+            {
+                a += t + ", ";
+            }
+
+            Recorder.Stop();
+
+            Recorder.Start();
+            Console.WriteLine("Using StringBuilder");
+            var builder = new StringBuilder();
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                builder.Append(numbers[i]);
+                builder.Append(", ");
+            }
+
+            Recorder.Stop();
+
+            Recorder.Start();
+            Console.WriteLine("Using StringBuilder (foreach)");
+            var builder2 = new StringBuilder();
+            foreach (var number in numbers)
+            {
+                builder2.Append(number);
+                builder2.Append(", ");
+            }
+
+            Recorder.Stop();
+        }
+    }
+}
---
+++ MonitoringLib/Recorder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+using static System.Diagnostics.Process;
+
+namespace MonitoringLib
+{
+    public static class Recorder
+    {
+        private static readonly Stopwatch _timer = new Stopwatch();
+        private static long _bytesPhysicalBefore = 0;
+        private static long _bytesVirtualBefore = 0;
+
+        public static void Start()
+        {
+            // force two garbage collections to release memory that is no longer references but has not been released yet
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            // store the current physical and virtual memory use
+            _bytesPhysicalBefore = GetCurrentProcess().WorkingSet64;
+            _bytesVirtualBefore = GetCurrentProcess().VirtualMemorySize64;
+            _timer.Restart();
+        }
+
+        public static void Stop()
+        {
+            _timer.Stop();
+            var bytesPhysicalAfter = GetCurrentProcess().WorkingSet64;
+            var bytesVirtualAfter = GetCurrentProcess().VirtualMemorySize64;
+
+            WriteLine("{0:N0} physical bytes used.", bytesPhysicalAfter - _bytesPhysicalBefore);
+            WriteLine("{0:N0} virtual bytes used.", bytesVirtualAfter - _bytesVirtualBefore);
+
+            WriteLine("{0} time span elapsed.", _timer.Elapsed);
+
+            WriteLine("{0:N0} total milliseconds elapsed.", _timer.ElapsedMilliseconds);
+        }
+    }
+}

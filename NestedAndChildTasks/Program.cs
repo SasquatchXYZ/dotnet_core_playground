70200b254750aaed7a275325b05d53c165ade376
Dalton Ricker <dricker@wayfair.com>
2020-08-28 17:18:02 -0400

    added nested and child tasks project and working on sychronizing resource access

---
+++ NestedAndChildTasks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace NestedAndChildTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var outer = Task.Factory.StartNew(OuterMethod);
+            outer.Wait();
+            WriteLine("Console app is stopping.");
+        }
+
+        static void OuterMethod()
+        {
+            WriteLine("Outer Method starting...");
+            var inner = Task.Factory.StartNew(InnerMethod, TaskCreationOptions.AttachedToParent);
+            // inner.Wait();
+            WriteLine("Outer Method finished.");
+        }
+
+        static void InnerMethod()
+        {
+            WriteLine("Inner Method starting...");
+            Thread.Sleep(2_000);
+            WriteLine("Inner Method finished.");
+        }
+    }
+}

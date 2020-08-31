fea3c9f21d45adb6c93b62d24b89be9b56dbca40
Dalton Ricker <dricker@wayfair.com>
2020-08-31 17:09:25 -0400

    added async console and enumerable

---
+++ AsyncConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AsyncConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Magenta;
+
+            var client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync("https://www.apple.com/");
+
+            WriteLine("Apple's homepage has {0:N0} bytes.", response.Content.Headers.ContentLength);
+        }
+    }
+}
---
+++ AsyncEnumerable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AsyncEnumerable
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.DarkCyan;
+
+            await foreach (int number in GetNumbers())
+            {
+                WriteLine($"Number: {number}");
+            }
+        }
+
+        static async IAsyncEnumerable<int> GetNumbers()
+        {
+            var r = new Random();
+
+            // simulate work
+            await Task.Run(() => Task.Delay(r.Next(1_500, 3_000)));
+            yield return r.Next(1, 101);
+
+            await Task.Run(() => Task.Delay(r.Next(1_500, 3_000)));
+            yield return r.Next(1, 101);
+
+            await Task.Run(() => Task.Delay(r.Next(1_500, 3_000)));
+            yield return r.Next(1, 101);
+        }
+    }
+}
